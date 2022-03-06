50f25e236e1eaab4ff9b55d2a745335c55b8c825
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2022-03-06 23:33:21 +0100

    Create main.dart

---
+++ test_script/lib/main.dart
@@ -0,0 +1,9 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'ui/home.dart';
+//void main () => runApp(home());
+
+void main() => runApp(new MaterialApp(
+      home: scaffoldex(),
+    ));
